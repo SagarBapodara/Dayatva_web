27b3709f93a4d9d9008ec7647483f4de292516fd
SagarBapodara <56546128+SagarBapodara@users.noreply.github.com>
2020-03-08 16:18:13 +0530

    added login

---
+++ index_login.js
@@ -0,0 +1,44 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("login_div").style.display = "none";
+    document.getElementById("user_div").style.display = "block";
+
+    var user = firebase.auth().currentUser;
+    if(user!=null){
+    	var email_id = user.email;
+    	document.getElementById("user_para").innerHTML = "Welcome User : " + email_id
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").styledisplay = "block";
+  }
+});
+
+
+function login(){
+
+	var userEmail = document.getElementById('email_field').value;
+	var userPass = document.getElementById('password_field').value;
+
+	firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) 
+	{
+		
+		var errorCode = error.code;
+		var errorMessage = error.message;
+
+	    window.alert("Error : " + errorMessage);
+		// ...   
+	});
+}
+
+function logout(){
+
+	firebase.auth().signOut();
+	document.getElementById("login_div").style.display = "block";
+}
